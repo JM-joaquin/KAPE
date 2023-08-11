2324d382498ab94f31ad99fce4ac425d56b8ae58
Facundo Janusa <facundojanusa@gmail.com>
2023-08-11 16:23:49 -0300

    [FIX] agrego implementaciones de Backend

--- Backend/app.py
+++ Backend/app.py
@@ -1,13 +1,13 @@
+from utils.db import db  # Importa db aquí
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from routes.auth import auth
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost:3308/kape'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost:3306/kape'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-from utils.db import db  # Importa db aquí
 
 db.init_app(app)  # Configura db con la aplicación
 
